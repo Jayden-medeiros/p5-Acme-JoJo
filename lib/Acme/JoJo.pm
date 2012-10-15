a60b7c80c126440d59de30e04762297b4e3094ca
dameninngenn <dameninngenn.owata@gmail.com>
2012-10-15 12:39:19 +0900

    initial commit

---
+++ lib/Acme/JoJo.pm
@@ -0,0 +1,32 @@
+package Acme::JoJo;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::JoJo -
+
+=head1 SYNOPSIS
+
+  use Acme::JoJo;
+
+=head1 DESCRIPTION
+
+Acme::JoJo is
+
+=head1 AUTHOR
+
+dameninngenn E<lt>dameninngenn.owata {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
